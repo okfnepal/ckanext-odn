8ef2dd4b8559a8cadd32c01d39c58ed0d7452d21
Sagar Ghimire <info@sagarg.com.np>
2020-04-11 11:55:05 +0545

    [THEME][XL]: templating and styling - theming setup

--- ckanext/oknp/plugin.py
+++ ckanext/oknp/plugin.py
@@ -6,8 +6,8 @@ class OknpPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
 
     # IConfigurer
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'oknp')
+        
