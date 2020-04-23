39e71d0655516ea0a9d62570fb72b7c4306a41a3
Sagar Ghimire <info@sagarg.com.np>
2020-04-23 07:56:56 +0545

    [xs]: group facet remved can be reverted

--- ckanext/oknp/plugin.py
+++ ckanext/oknp/plugin.py
@@ -11,7 +11,6 @@ _ = toolkit._
 class OknpPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.ITemplateHelpers, inherit=True)
-    plugins.implements(plugins.IFacets)
     plugins.implements(plugins.IGroupController)
     plugins.implements(plugins.IRoutes, inherit=True)
     plugins.implements(plugins.IAuthFunctions)
@@ -22,19 +21,6 @@ class OknpPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'oknp')
-
-    # IFacets   
-    def _facets(self, facets_dict):
-        if 'groups' in facets_dict:
-            del facets_dict['groups']
-        return facets_dict
-
-    def dataset_facets(self, facets_dict, package_type):
-        return self._facets(facets_dict)
-
-    def organization_facets(self, facets_dict, organization_type,
-            package_type):
-        return self._facets(facets_dict)
 
     # IGroupController 
     def before_view(self, pkg_dict):
